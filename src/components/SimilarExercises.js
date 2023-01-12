880ee385c7b0908d4a8bce1cc98757fc44d90ca1
Hasan İzmir <hasanizmirr@gmail.com>
2023-01-12 11:11:06 +0300

    feat:SimilarExercises component completed

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const SimilarExercises = () => {
+import HorizontalScrollbar from "./HorizontalScrollbar";
+import Loader from "./Loader";
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
-    <div>SimilarExercises</div>
-  )
-}
+    <Box sx={{ mt: { lg: "100px", xs: "0" } }}>
+      <Typography variant="h3" mb={5}>
+        Exercises that target the
+        <span style={{ color: "#ff2625" }}> same muscle group</span>
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {targetMuscleExercises.length ? (
+          <HorizontalScrollbar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+      <Typography variant="h3" mt={20} mb={5}>
+        Exercises that use the
+        <span style={{ color: "#ff2625" }}> same equipment</span>
+      </Typography>
+      <Stack direction="row" sx={{ p: "2", position: "relative" }}>
+        {equipmentExercises.length ? (
+          <HorizontalScrollbar data={equipmentExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
+    </Box>
+  );
+};
 
-export default SimilarExercises
+export default SimilarExercises;
